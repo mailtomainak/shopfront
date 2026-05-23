905dc11df82927294d994eb8fdb2e71942779ae6
gardener-3 <gardener-3@gmail.com>
2026-05-23 17:43:12 +0200

    Add Customer and Order identities

---
+++ src/Ordering/Ordering.Domain/Customers/CustomerId.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Domain.Customers
+{
+    public readonly record struct CustomerId(Guid Value)
+    {
+        public static CustomerId New() => new(Guid.NewGuid());
+        public override string ToString() => Value.ToString();
+    }
+}
---
+++ src/Ordering/Ordering.Domain/Orders/OrderId.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Domain.Orders
+{
+   public readonly record struct OrderId(Guid Value)
+    {
+        public static OrderId New() => new(Guid.NewGuid());
+        public override string ToString() => Value.ToString();        
+    }
+}
